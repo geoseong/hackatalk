ccab3b687bb4aafff247d2c2a02859394c40f9d1
TaeSeong Park <imf4@naver.com>
2020-08-03 22:21:53 +0900

    Implement user connectionPlugin refer to https://github.com/prisma/prisma/discussions/3190 & remain TODO

--- server/src/types/resolvers/User/query.ts
+++ server/src/types/resolvers/User/query.ts
@@ -1,5 +1,7 @@
+import { queryField, stringArg } from '@nexus/schema';
+
+import { FindManyUserArgs } from '@prisma/client';
 import { getUserId } from '../../../utils/auth';
-import { queryField } from '@nexus/schema';
 
 export const me = queryField('me', {
   type: 'User',
@@ -15,4 +17,47 @@ export const me = queryField('me', {
       },
     });
   },
+});
+
+export const usersQueryField = queryField((t) => {
+  t.connectionField('users', {
+    type: 'User',
+    additionalArgs: {
+      email: stringArg(),
+      name: stringArg(),
+    },
+    async nodes(_, args, ctx) {
+      const { first, after, before, last, email, name } = args;
+
+      console.log('args', args);
+
+      const defaultFindManyParams = {
+        where: {
+          email: {
+            contains: email,
+          },
+          name: {
+            contains: name,
+          },
+          deletedAt: null,
+        },
+      };
+
+      const cursorParam =
+        after || before ? { cursor: { id: after || before } } : {};
+      // Important to always fetch one more item so that we can know if there's a next page
+      // TODO: deal with last args
+      const takeParam = first || last ? { take: (first || last) + 1 } : {};
+      const findManyParams: FindManyUserArgs = {
+        ...cursorParam,
+        ...takeParam,
+        ...defaultFindManyParams,
+      };
+
+      console.log('findManyParams', findManyParams)
+
+      const users = await ctx.prisma.user.findMany(findManyParams);
+      return users;
+    },
+  });
 });
